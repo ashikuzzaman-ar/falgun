78354239fab1b2ad0c6eafdd4a924db529ff65c8
ashik <ashik@studevs>
2016-04-14 04:12:18 +0600

    Successfully Created ObjectSerialization class for serializing and deserializing an Object.

---
+++ src/com/studevs/io/ObjectSerialization.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.studevs.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author ashik
+ */
+public class ObjectSerialization {
+
+    private Object serializedObject;
+    private FileOutputStream fileOutputStream;
+    private ObjectOutputStream objectOutputStream;
+    private FileInputStream fileInputStream;
+    private ObjectInputStream objectInputStream;
+
+    public ObjectSerialization(Object object) {
+
+        try {
+
+            this.serializedObject = object;
+        } catch (Exception e) {
+
+            System.err.println(e.toString());
+        }
+    }
+
+    public void setObjectToSerialize(Object object) {
+
+        try {
+
+            this.serializedObject = object;
+        } catch (Exception e) {
+
+            System.err.println(e.toString());
+        }
+    }
+
+    public void serialize() {
+
+        try {
+
+            this.fileOutputStream = new FileOutputStream("object.ser");
+            this.objectOutputStream = new ObjectOutputStream(this.fileOutputStream);
+
+            this.objectOutputStream.writeObject(this.serializedObject);
+
+            this.objectOutputStream.close();
+            this.fileOutputStream.close();
+
+        } catch (Exception e) {
+
+            System.err.println(e.toString());
+        }
+    }
+
+    public Object getSerializedObject() {
+
+        try {
+
+            this.fileInputStream = new FileInputStream("object.ser");
+            this.objectInputStream = new ObjectInputStream(fileInputStream);
+            this.serializedObject = this.objectInputStream.readObject();
+
+            this.objectInputStream.close();
+            this.fileInputStream.close();
+            
+            return this.serializedObject;
+        } catch (IOException | ClassNotFoundException e) {
+
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+    
+    public void removeSerializationFile(){
+        
+        try {
+            
+            new File("object.ser").delete();
+        } catch (Exception e) {
+            
+            System.err.println(e.toString());
+        }
+    }
+}
