e2ac161fb77656d7f5edc9986b0c39ea83f16495
ashik <ashik@studevs>
2016-04-14 06:27:25 +0600

    Successfully Created ObjectCopy class and DeepCopy interface for cloning an Object.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
-import com.studevs.util.RandomNumberGenerator;
+import com.studevs.util.DeepCopy;
+import com.studevs.util.ObjectCopy;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,12 +13,27 @@ import com.studevs.util.RandomNumberGenerator;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
-        RandomNumberGenerator rng = new RandomNumberGenerator(500000000);
-        for (int i = 0; i < 100; i++) {
+        Test t = new Test();
+        t.setA(10);
+        Test t2 = (Test) ObjectCopy.getCopyOf(t);
+        t.setA(15);
+        
+        System.out.println("Value of t: "+t.getA());
+        System.out.println("Value of t2: "+t2.getA());
+    }
+}
+//You have to implement DeepCopy Only if you want to make a class cloneable
+class Test implements DeepCopy{
 
-            System.out.println(rng.getGeneratedNumber());
-        }
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
     }
 }
---
+++ src/com/studevs/util/ObjectCopy.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.studevs.util;
+
+import com.studevs.io.ObjectSerialization;
+
+/**
+ *
+ * @author ashik
+ */
+public class ObjectCopy {
+
+    public static Object getCopyOf(Object object){
+        
+        try {
+            
+            ObjectSerialization objectSerialization = new ObjectSerialization(object);
+            objectSerialization.serialize();
+            
+            object = objectSerialization.getSerializedObject();
+            
+            objectSerialization.removeSerializationFile();
+            
+            return object;
+        } catch (Exception e) {
+            
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+}
